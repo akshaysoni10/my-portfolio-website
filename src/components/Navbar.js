5ce2a33887841ed463fd32bd657d8b3b18d11965
Akshay Soni <109035961+akshaysoni10@users.noreply.github.com>
2024-03-25 19:32:06 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -102,7 +102,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/akshaysoni10/Portfolio-Website"
+                href="https://github.com/akshaysoni10/my-portfolio-website"
                 target="_blank"
                 className="fork-btn-inner"
               >
